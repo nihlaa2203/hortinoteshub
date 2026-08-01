1080996724493e9b4735a5a4b1ce270436dd0860
nihlaa2203 <nihlaa2203@gmail.com>
2026-08-01 17:16:35 +0530

    new script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-alert("JavaScript Loaded!");
+const toggle = document.getElementById("theme-toggle");
+
+console.log("Script Loaded");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggle.checked = true;
+    console.log("Dark mode loaded from localStorage");
+}
+
+toggle.addEventListener("change", () => {
+    console.log("Toggle clicked!");
+
+    if (toggle.checked) {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark");
+        console.log("Dark mode ON");
+    } else {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("theme", "light");
+        console.log("Dark mode OFF");
+    }
+});
